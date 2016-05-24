36b97030ee068c895ee168758b48df3d38bc5c3c
Alex Slaughter <alex@alexslaughter.com>
2016-05-24 18:11:37 -0400

    add support for accessing properties

---
+++ src/IntercomLaravel.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Shadow\IntercomLaravel;
+
+use Intercom\IntercomClient;
+
+class IntercomLaravel
+{
+    private $intercom;
+    public function __construct($id, $key)
+    {
+        $this->intercom = new IntercomClient($id, $key);
+    }
+
+    public function __call($method, array $args)
+    {
+        if (method_exists($this->intercom, $method)) {
+            return $this->intercom->$method(...$args);
+        }
+
+        return $this->intercom->$method;
+    }
+}
--- src/ServiceProvider.php
+++ src/ServiceProvider.php
@@ -4,17 +4,16 @@ namespace Shadow\IntercomLaravel;
 
 use Illuminate\Support\Facades\Config;
 use Illuminate\Support\ServiceProvider as IlluminateServiceProvider;
-use Intercom\IntercomClient;
 
 class ServiceProvider extends IlluminateServiceProvider
 {
     public function register()
     {
         $this->app->bind('intercom', function () {
-            $id = Config::get('services.intercom.appId');
-            $key = Config::get('services.intercom.appKey');
+            $id = Config::get('services.intercom.app_id');
+            $key = Config::get('services.intercom.api_key');
 
-            return new IntercomClient($id, $key);
+            return new IntercomLaravel($id, $key);
         });
     }
 }
